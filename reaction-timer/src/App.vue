4ab6b226d4a0ed03f5138e44c379b95ad4678091
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-12 10:37:08 +0200

    æå

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -2,29 +2,35 @@
 <h1>Scholar Reaction Timer</h1>
 <button @click="start" :disabled="isPlaying">Play</button>
 <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+<Resaults v-if="showResults" :score="score"/>
+<!-- <p v-if="showResults">Reaction time: {{ score }}ms</p> -->
 
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Resaults from './components/Resaults.vue'
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Resaults },
   data() {
     return{
       isPlaying: false,
       delay: null,
       score: null,
+      showResults: false,
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000 
       this.isPlaying = true
+      this.showResults = false
     },
     endGame(reactionTime){
       this.score = reactionTime
       this.isPlaying = false
+      this.showResults = true
     }
   }
 }
